54f3b56d8161acb2128441f4c2dde73f6cba0daa
glaslos <lukas_rist@symantec.com>
2017-02-07 15:30:39 +0100

    comparing hashes

--- ssdeep_test.go
+++ ssdeep_test.go
@@ -1,7 +1,6 @@
 package ssdeep
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 	"os/exec"
@@ -18,17 +17,9 @@ func TestRollingHash(t *testing.T) {
 func TestFindBlock(t *testing.T) {
 	sdeep := NewSSDEEP()
 	sdeep.Fuzzy("/tmp/data")
-	out, err := exec.Command("ssdeep", "/tmp/data").Output()
+	_, err := exec.Command("ssdeep", "/tmp/data").Output()
 	if err != nil {
 		log.Fatal(err)
-	}
-	fmt.Println(string(out))
-}
-
-func TestKittenDistance(t *testing.T) {
-	d := Distance("kitten", "sitting")
-	if d != 3 {
-		t.Errorf("Invalid edit distance: %d", d)
 	}
 }
 
@@ -41,7 +32,7 @@ func BenchmarkRollingHash(b *testing.B) {
 
 func BenchmarkDistance(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Distance("aaa123123123123aaaa", "aab123123123aaaa")
+		distance("aaa123123123123aaaa", "aab123123123aaaa")
 	}
 }
 
